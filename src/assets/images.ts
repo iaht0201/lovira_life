fb6cdf1eea3d807870076d8447a589a911fb8738
Doan Quang Thai <64180813+iaht0201@users.noreply.github.com>
2026-08-24 22:58:27 +0700

    update binary64 banner

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,11 +1,11 @@
 import avatarImg from './images/avatar.png';
-import bannerImg from './images/banner.png';
 import logoImg from './images/logo.png';
 import logoClientImg from './images/logo_client.png';
+import bannerTxt from '../../public/banner.txt?raw';
 
 export const APP_IMAGES = {
   avatar: avatarImg,
-  banner: bannerImg,
+  banner: bannerTxt.trim(),
   logo: logoImg,
   logoIcon: logoImg,
   logoClient: logoClientImg,
